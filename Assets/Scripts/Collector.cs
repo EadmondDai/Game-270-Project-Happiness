4ec4c6b502bdf1a51d3bcfca2e9ba02df33d259f
BuyMeAnIcecream <diffrectingbull@gmail.com>
2016-10-24 14:47:14 -0700

    a

---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collector: MonoBehaviour {
+    public int partsToCollect;
+    public ParticleSystem fog;
+    private float densityStep;
+    int collectedParts;
+    float maxDensity;
+    // Use this for initialization
+	void Start () {
+        collectedParts = 0;
+        maxDensity = fog.startSize;
+        densityStep = maxDensity / partsToCollect;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if( col.tag == "Friend")
+        {
+            changeFogDensity(-densityStep); 
+        }
+        //todo
+        if (col.tag == "Enemy") {
+            changeFogDensity(densityStep);
+        }
+    }
+
+    void changeFogDensity(float changeBy)
+    {
+        if (fog.startSize + changeBy < 0)
+            fog.startSize = 0;
+        else if (fog.startSize + changeBy > maxDensity)
+            fog.startSize = maxDensity;
+        else
+            fog.startSize += changeBy;
+    }
+}
---
+++ Assets/Scripts/Communicate.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Communicate : MonoBehaviour {
+    //Behaviour halo;
+    Light halo;
+    public float fadeSpeed;
+    bool fading;
+   // bool faded;
+    public bool Fading
+    {
+        get { return fading; }
+        set { fading = value; }
+    }
+	void Start () {
+        // halo = (Behaviour)GetComponent("Halo");
+        halo = GetComponent<Light>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (fading)
+            haloFade();
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+            fading = true;
+            // halo.enabled = false;
+            //haloFade();
+    }
+   
+    void haloFade()
+    {
+//        if (halo = GetComponent<Light>()) Debug.Log("niccce");
+        halo.intensity -= fadeSpeed * Time.deltaTime;
+        halo.range -= fadeSpeed * Time.deltaTime;
+        if (halo.range <= 0)
+            fading = false;
+            //faded = true;
+        
+        //Debug.Log(halo.GetType().GetProperty("size"));//.SetValue(halo, false, null);
+    }
+}
