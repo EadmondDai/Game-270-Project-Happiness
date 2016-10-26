c99342449c1fca9ce923426b62a23be97a74fabe
BuyMeAnIcecream <diffrectingbull@gmail.com>
2016-10-25 22:07:05 -0700

    fixed fog, added sound manger

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -7,10 +7,12 @@ public class Collector: MonoBehaviour {
     private float densityStep;
     int collectedParts;
     float maxDensity;
-    // Use this for initialization
+    
+        // Use this for initialization
 	void Start () {
         collectedParts = 0;
-        maxDensity = fog.startSize;
+        maxDensity = fog.emissionRate;
+        //maxDensity = fog.startSize;
         densityStep = maxDensity / partsToCollect;
 	}
 	
@@ -30,7 +32,16 @@ public class Collector: MonoBehaviour {
             changeFogDensity(densityStep);
         }
     }
-
+     void changeFogDensity(float changeBy)
+    {
+        if (fog.emissionRate + changeBy < 0)
+            fog.emissionRate = 0;
+        else if (fog.emissionRate + changeBy > maxDensity)
+            fog.emissionRate = maxDensity;
+        else
+            fog.emissionRate += changeBy;
+    }
+    /*
     void changeFogDensity(float changeBy)
     {
         if (fog.startSize + changeBy < 0)
@@ -40,4 +51,5 @@ public class Collector: MonoBehaviour {
         else
             fog.startSize += changeBy;
     }
+    */
 }
